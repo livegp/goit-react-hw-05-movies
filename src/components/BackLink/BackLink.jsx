32351bf6ba9961cce6345edf0aa98ce8e12a46ce
Oleksandr <live.gp@gmail.com>
2023-08-09 19:31:07 +0300

    Update BackLink.jsx

--- src/components/BackLink/BackLink.jsx
+++ src/components/BackLink/BackLink.jsx
@@ -2,12 +2,11 @@ import { IoChevronBack } from 'react-icons/io5';
 
 import { LinkContainer, StyledLink } from './BackLink.styled';
 
-function BackLink({ to, children }) {
+function BackLink({ to }) {
   return (
     <StyledLink to={to}>
       <LinkContainer>
         <IoChevronBack size="24" />
-        {children}
         <p>Back</p>
       </LinkContainer>
     </StyledLink>
