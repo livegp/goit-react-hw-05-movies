2de221fc01faa15d49bb96902c5a17545b8884d7
Oleksandr <live.gp@gmail.com>
2023-11-04 12:31:55 +0200

    Create ToggleTheme.styled.js

---
+++ src/components/ToggleTheme/ToggleTheme.styled.js
@@ -0,0 +1,45 @@
+import { FaMoon, FaSun } from 'react-icons/fa6';
+import styled, { keyframes } from 'styled-components';
+
+export const LabelToggleTheme = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  order: 2;
+  cursor: pointer;
+  user-select: none;
+
+  svg {
+    font-size: 30px;
+    fill: rgba(var(--bg-primary), 0.7);
+    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  :hover svg {
+    fill: rgba(var(--bg-primary), 1);
+  }
+
+  input {
+    cursor: pointer;
+    opacity: 0;
+  }
+`;
+
+const rotateslide = keyframes`   
+  0% {
+    transform: rotate(-360deg) scale(0);
+    opacity: 0;
+  }
+
+  75% {
+    transform: rotate(25deg);
+  }
+`;
+
+export const MoonToggleTheme = styled(FaMoon)`
+  animation: ${rotateslide} 0.5s;
+`;
+
+export const SunToggleTheme = styled(FaSun)`
+  animation: ${rotateslide} 0.5s;
+`;
