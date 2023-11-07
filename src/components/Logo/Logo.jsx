4f9472e3d40d2acef31ccbed8aac1642fd9c2a2f
Oleksandr <live.gp@gmail.com>
2023-11-07 23:05:52 +0200

    fix logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,29 @@
+import { useState, useEffect } from 'react';
 import { RiMovie2Line } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
 import LogoBox from './Logo.styled';
 
 function Logo() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <LogoBox>
       <Link to="/">
         <RiMovie2Line />
-        <p>Movies</p>
+        {windowWidth >= 600 && <p>Movies</p>}
       </Link>
     </LogoBox>
   );
